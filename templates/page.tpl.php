f57ea651d1fcfa5abb4a830dfe18cd2a9529d41c
Renaud MATHIEU <renaudmathieu@MacBook-Pro-de-Renaud-Mathieu.local>
2012-01-10 20:00:12 -0500

    [DEVEL] page template skeleton implemented

---
+++ templates/page.tpl.php
@@ -0,0 +1,81 @@
+<div id="header">
+
+    <div id="header-inside">
+
+        <div id="header-inside-left">     
+            header-inside-left
+        </div>
+
+        <div id="header-inside-right">
+            header-inside-right   
+        </div>
+
+    </div>
+
+</div>
+
+
+<div id="header-menu">
+
+    <div id="header-menu-inside">
+        header-menu-inside
+    </div>
+
+</div>
+
+<div id="banner">
+    banner
+</div>
+
+<div id="content">
+
+    <div id="content-inside" class="inside">
+
+        <div id="main">
+            main
+        </div>
+
+        <div id="sidebar">
+            sidebar
+        </div>
+
+    </div>
+
+</div>
+
+<div id="footer">
+
+    <div id="footer-inside">
+
+        <div class="footer-area first">
+            footer-area first
+        </div>
+
+        <div class="footer-area second">
+            footer-area second
+        </div>
+
+        <div class="footer-area third">
+            footer-area third
+        </div>
+
+    </div>
+
+</div>
+
+<div id="footer-bottom">
+
+    <div id="footer-bottom-inside">
+
+        <div id="footer-bottom-left">
+            footer-bottom-left
+        </div>
+
+        <div id="footer-bottom-right">
+            footer-bottom-right
+        </div>
+
+    </div>
+
+</div>
+
