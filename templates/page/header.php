be0bece8a5c1026f1231f43c38d1105b85132e4e
Renaud MATHIEU <renaudmathieu@192.168.1.110>
2012-01-18 23:14:39 -0500

    [DEVEL] .gitignore

--- templates/page/header.php
+++ templates/page/header.php
@@ -3,9 +3,23 @@
     <div id="header-inside">
 
         <div id="header-inside-left">     
+
             <?php if ($logo): ?>
-            <a href="<?php print check_url($front_page); ?>" title="<?php print t('Home'); ?>"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
+                <a href="<?php print check_url($front_page); ?>" title="<?php print t('Home'); ?>"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
             <?php endif; ?>
+
+            <?php if ($site_name || $site_slogan): ?>
+                <div class="clearfix">
+                    <?php if ($site_name): ?>
+                        <span id="site-name"><a href="<?php print check_url($front_page); ?>" title="<?php print t('Home'); ?>"><?php print $site_name; ?></a></span>
+                    <?php endif; ?>
+                    <?php if ($site_slogan): ?>
+                        <span id="slogan"><?php print $site_slogan; ?></span>
+                    <?php endif; ?>
+                </div>
+            <?php endif; ?>
+
+
         </div>
 
         <div id="header-inside-right">
